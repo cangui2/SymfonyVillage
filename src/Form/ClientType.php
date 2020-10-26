09740afc86f046892c86886c8b08f8945989f612
cangui <canguijc@gmail.com>
2020-10-26 17:00:56 +0100

    Version beta 2.01

---
+++ src/Form/ClientType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('utilNom')
+            ->add('utilPrenom')
+            ->add('utilSexe')
+            ->add('utilAdresse')
+            ->add('utilCp')
+            ->add('utilVille')
+            ->add('utilMail')
+            ->add('utilUsername')
+            ->add('utilPassword')
+            ->add('utilTelephone')
+            ->add('utilDateDeNaissance',DateType::class,[
+                'placeholder' => 'Select a value',
+            ])
+            
+            ->add('cliAdresseLivraison')
+            ->add('cliType')
+            ->add('cliCoefficient')
+            ->add('cliReference')
+            ->add('com')
+            ->add('pay')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
