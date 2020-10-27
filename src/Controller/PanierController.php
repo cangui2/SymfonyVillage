85477316b93ffbc241b46876f37ab34d31042255
cangui <canguijc@gmail.com>
2020-10-27 09:42:06 +0100

    version beta 2.1

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,73 +2,47 @@
 
 namespace App\Controller;
 
-use App\Repository\ProduitRepository;
-use SessionIdInterface;
-use Symfony\Component\HttpFoundation\Request;
+
+use App\Service\Panier\PanierService;
+
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class PanierController extends AbstractController
 {
     /**
      * @Route("/panier", name="panier")
      */
-    public function index(SessionInterface $session, ProduitRepository $produitRepository)
+    public function index(PanierService $panierService )
     {
-        $panier=$session->get('panier',[]);
-        $panierWhithData=[];
-
-        foreach ($panier as $id => $quantity){
-            $panierWhithData[]= [
-                'produit'=> $produitRepository->find($id),
-                'quantity'=>$quantity
-
-            ];
-        }
        
-        $total=0;
-         foreach ($panierWhithData as $item ) {
-            $totalItem=$item['produit']->getProPrix()*$item['quantity'];
-            $total +=$totalItem;
-         }
-
+      
         return $this->render('panier/index.html.twig', [
-            'item' =>$panierWhithData,
-            'total'=>$total
+            'item' =>$panierService->getFullPanier(),
+            'total'=>$panierService->getTotal()
         ]);
     }
     /**
      * @Route("/panier/add/{id}", name="panier_add")
      */
-    public function add($id,SessionInterface $session){
+    public function add($id,PanierService $panierService){
 
-            
-            $panier=$session->get('panier',[]);
-            if (!empty($panier[$id])){
-                $panier[$id]++;
-            }else{
-                $panier[$id]=1;
-            }
+        $panierService->add($id);
 
-            
-
-            $session->set('panier',$panier);
+           
            
             return $this->redirectToRoute("panier");
     }
     /**
      * @Route("/panier/supprimer/{id}", name="panier_remove")
      */
-    public function remove($id, SessionInterface $session){
+    public function remove($id, PanierService $panierService){
 
-        $panier =$session ->get('panier',[]);
+        $panierService->remove($id);
 
-        if (!empty($panier[$id])){
-            unset($panier[$id]);
-        }
-        $session->set('panier',$panier);
+
         return $this->redirectToRoute("panier");
     }
 }
---
+++ src/Service/Panier/PanierService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Service\Panier;
+
+use App\Repository\ProduitRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class PanierService{
+
+   protected $_session;
+   protected $_produitRepository;
+
+   public function __construct(SessionInterface $session,ProduitRepository $produitRepository)
+   {
+       $this->_session=$session;
+       $this->_produitRepository=$produitRepository;
+   }
+
+
+    public function add(int $id){
+
+        $panier=$this->_session->get('panier',[]);
+        if (!empty($panier[$id])){
+            $panier[$id]++;
+        }else{
+            $panier[$id]=1;
+        }
+
+        
+
+        $this->_session->set('panier',$panier);
+    }
+
+    public function remove(int $id){
+        $panier =$this->_session->get('panier',[]);
+
+        if (!empty($panier[$id])){
+            unset($panier[$id]);
+        }
+        $this->_session->set('panier',$panier);
+
+    }
+    
+    public function getFullPanier(): array {
+        $panier=$this->_session->get('panier',[]);
+        $panierWhithData=[];
+
+        foreach ($panier as $id => $quantity){
+            $panierWhithData[]= [
+                'produit'=> $this->_produitRepository->find($id),
+                'quantity'=>$quantity
+
+            ];
+        }
+            return $panierWhithData;
+    }
+    public function getTotal():float{
+        $total=0;
+        
+        foreach ($this->getFullPanier() as $item ) {
+           $totalItem=$item['produit']->getProPrix()*$item['quantity'];
+           $total +=$totalItem;
+        }
+        return $total;
+        
+    }
+
+
+}
