9ac86035253f7c9254c9b09d5d5dcef1d45754ac
Your Name <you@example.com>
2020-11-09 09:29:16 +0100

    Version 0.1

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,13 @@ $(document).ready(function () {
  
 const button = document.querySelector('#button');
 const tooltip = document.querySelector('#tooltip');
+const button2 = document.querySelector('#button2');
+const tooltip2 = document.querySelector('#tooltip2');
+const button3 = document.querySelector('#button3');
+const tooltip3 = document.querySelector('#tooltip3');
 const body = document.querySelector('header');
-
+const nav=document.querySelector('.item');
+const nav2=document.querySelector('.batterie');
 let popperInstance = null;
 
 function create() {
@@ -19,6 +24,31 @@ function create() {
       },
     ],
   });
+  
+}
+function create2(){
+  popperInstance = Popper.createPopper(button2, tooltip2, {
+    modifiers: [
+      {
+        name: 'offset2',
+        options: {
+          offset: [-50,-10],
+        },
+      },
+    ],
+  });
+}
+  function create3(){
+    popperInstance = Popper.createPopper(button3, tooltip3, {
+      modifiers: [
+        {
+          name: 'offset3',
+          options: {
+            offset: [-50,-10],
+          },
+        },
+      ],
+    });
 }
 
 function destroy() {
@@ -27,15 +57,45 @@ function destroy() {
     popperInstance = null;
   }
 }
-
+function destroy2() {
+  if (popperInstance) {
+    popperInstance.destroy();
+    popperInstance = null;
+  }
+}
+function destroy3() {
+  if (popperInstance) {
+    popperInstance.destroy();
+    popperInstance = null;
+  }
+}
 function show() {
   tooltip.setAttribute('data-show', '');
   create();
+}
+function show2(){
+  tooltip2.setAttribute('data-show2', '');
+  create2();
+
+}
+function show3(){
+  tooltip3.setAttribute('data-show3', '');
+  create3();
+
 }
 
 function hide() {
   tooltip.removeAttribute('data-show');
   destroy();
+  
+}
+function hide2(){
+  tooltip2.removeAttribute('data-show2');
+  destroy2();
+}
+function hide3(){
+  tooltip3.removeAttribute('data-show3');
+  destroy3();
 }
 
 const showEvents = ['mouseenter', 'focus'];
@@ -43,13 +103,21 @@ const hideEvents = ['mouseleave', 'blur'];
 
 showEvents.forEach(event => {
   button.addEventListener(event, show);
- 
+  button2.addEventListener(event, show2);
+  button3.addEventListener(event, show3);
 });
 
 hideEvents.forEach(event => {
   body.addEventListener(event, hide);
-
+  nav.addEventListener(event, hide2);
+  nav2.addEventListener(event, hide3);
 });
+
+
+
+
+
+
 /*
 hideEvents.forEach(event => {
   tooltip.addEventListener(event, hide);
